b1e39bed0108d17d3460ea3018e043803d41e344
HieuDinh3322 <145694591+HieuDinh3322@users.noreply.github.com>
2023-11-25 23:50:51 +0700

    Add files via upload

---
+++ BleRG/BleRG.ino
@@ -0,0 +1,177 @@
+#include "BLEDevice.h"
+
+// Define UUIDs:
+static BLEUUID serviceUUID("4fafc201-1fb5-459e-8fcc-c5c9c331914b");
+static BLEUUID charUUID("beb5483e-36e1-4688-b7f5-ea07361b26a8");
+static BLEUUID charUUID_2("1c95d5e3-d8f7-413a-bf3d-7a2e5d7be87e");
+
+// Some variables to keep track of the device connection
+static boolean doConnect = false;
+static boolean connected = false;
+static boolean doScan = false;
+boolean sendAck = false;
+
+// Define pointer for the BLE connection
+static BLEAdvertisedDevice* myDevice;
+BLERemoteCharacteristic* pRemoteChar;
+BLERemoteCharacteristic* pRemoteChar_2;
+
+const int WINDOW_SIZE = 3;
+int expectedSeqNum = 0;
+
+class MyClientCallback : public BLEClientCallbacks {
+  void onConnect(BLEClient* pclient) {}
+
+  void onDisconnect(BLEClient* pclient) {
+    connected = false;
+    Serial.println("onDisconnect");
+  }
+};
+
+void sendAckNow() {
+  // uint8_t ackData[] = {static_cast<uint8_t>(ackNum)};
+  // pRemoteChar->writeValue(ackData, sizeof(ackData));
+  int value = 1;
+  String txValue = String(value);
+  Serial.println(txValue);
+  pRemoteChar_2->writeValue(txValue.c_str(), txValue.length());
+}
+
+// Callback function for Notify function
+static void notifyCallback(BLERemoteCharacteristic* pBLERemoteCharacteristic,
+                            uint8_t* pData,
+                            size_t length,
+                            bool isNotify) {
+  if(pBLERemoteCharacteristic->getUUID().toString() == charUUID.toString()) {
+
+    // convert received bytes to integer
+    // uint32_t counter = pData[0];
+    // for(int i = 1; i<length; i++) {
+    //   counter = counter | (pData[i] << i*8);
+    // }
+
+    // print to Serial
+    // Serial.print("Characteristic 1 (Notify) from server: ");
+    // Serial.println(counter );
+
+    Serial.print("Received bytes: ");
+    Serial.println(length);
+    
+    sendAck = true;  
+  }
+}
+
+// Function to chech Characteristic
+bool connectCharacteristic(BLERemoteService* pRemoteService, BLERemoteCharacteristic* l_BLERemoteChar) {
+  // Obtain a reference to the characteristic in the service of the remote BLE server.
+  if (l_BLERemoteChar == nullptr) {
+    Serial.print("Failed to find one of the characteristics");
+    Serial.print(l_BLERemoteChar->getUUID().toString().c_str());
+    return false;
+  }
+  Serial.println(" - Found characteristic: " + String(l_BLERemoteChar->getUUID().toString().c_str()));
+
+  if(l_BLERemoteChar->canNotify())
+    l_BLERemoteChar->registerForNotify(notifyCallback);
+
+  return true;
+}
+
+// Function that is run whenever the server is connected
+bool connectToServer() {
+  Serial.print("Forming a connection to ");
+  Serial.println(myDevice->getAddress().toString().c_str());
+  
+  BLEClient*  pClient  = BLEDevice::createClient();
+  Serial.println(" - Created client");
+
+  pClient->setClientCallbacks(new MyClientCallback());
+
+  // Connect to the remove BLE Server.
+  pClient->connect(myDevice);  // if you pass BLEAdvertisedDevice instead of address, it will be recognized type of peer device address (public or private)
+  Serial.println(" - Connected to server");
+
+  // Obtain a reference to the service we are after in the remote BLE server.
+  BLERemoteService* pRemoteService = pClient->getService(serviceUUID);
+  if (pRemoteService == nullptr) {
+    Serial.print("Failed to find our service UUID: ");
+    Serial.println(serviceUUID.toString().c_str());
+    pClient->disconnect();
+    return false;
+  }
+  Serial.println(" - Found our service");
+
+  connected = true;
+  pRemoteChar = pRemoteService->getCharacteristic(charUUID);
+  pRemoteChar_2 = pRemoteService->getCharacteristic(charUUID_2);
+  if(connectCharacteristic(pRemoteService, pRemoteChar) == false)
+    connected = false;
+  else if(connectCharacteristic(pRemoteService, pRemoteChar_2) == false)
+    connected = false;
+
+  if(connected == false) {
+    pClient-> disconnect();
+    Serial.println("At least one characteristic UUID not found");
+    return false;
+  }
+  return true;
+}
+
+// Scan for BLE servers and find the first one that advertises the service we are looking for.
+class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
+  //Called for each advertising BLE server.
+  void onResult(BLEAdvertisedDevice advertisedDevice) {
+    Serial.print("BLE Advertised Device found: ");
+    Serial.println(advertisedDevice.toString().c_str());
+  
+    // We have found a device, let us now see if it contains the service we are looking for.
+    if (advertisedDevice.haveServiceUUID() && advertisedDevice.isAdvertisingService(serviceUUID)) {
+  
+      BLEDevice::getScan()->stop();
+      myDevice = new BLEAdvertisedDevice(advertisedDevice);
+      doConnect = true;
+      doScan = true;
+  
+    } // Found our server
+  } // onResult
+}; // MyAdvertisedDeviceCallbacks
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Starting Arduino BLE Client application...");
+  BLEDevice::init("BLE client");
+  BLEScan* pBLEScan = BLEDevice::getScan();
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  pBLEScan->setInterval(1349);
+  pBLEScan->setWindow(449);
+  pBLEScan->setActiveScan(true);
+  pBLEScan->start(5, false);
+
+  Serial.println("Done Setup");
+}
+
+void loop() {
+  if (doConnect == true) {
+    if (connectToServer()) {
+      Serial.println("Connected to the BLE Server.");
+      connected = true;
+    } else {
+      Serial.println("Failed to connect to the server.");
+      connected = false;
+    }
+    doConnect = false;
+  }
+
+  if (connected) {
+    // Do something when connected
+    // Serial.println("Connected and waiting for notifications...");
+    if(sendAck){
+      sendAckNow();
+      sendAck = false;
+    }
+  } else if (doScan) {
+    BLEDevice::getScan()->start(0);
+  }
+
+  delay(1000);
+}
---
+++ BleSG/BleSG.ino
@@ -0,0 +1,183 @@
+#include <SPIFFS.h>
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+
+BLEServer* pServer = NULL;
+BLECharacteristic* pCharacteristic = NULL;
+BLECharacteristic* pCharacteristic_2 = NULL;
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+bool sendingFile = false;
+
+#define SERVICE_UUID          "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID   "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+#define CHARACTERISTIC_UUID_2 "1c95d5e3-d8f7-413a-bf3d-7a2e5d7be87e"
+BLE2902 *pBLE2902;
+BLE2902 *pBLE2902_2;
+
+unsigned long startMillis_1;
+unsigned long endMillis;          
+File file;
+
+// Global variables for ACK handling and retransmission
+volatile bool ackReceived = false;
+int retransmissionCount = 0;
+const int MAX_RETRANSMISSIONS = 3; // Set a maximum number of retransmissions
+String lastAckData;
+
+class MyServerCallbacks : public BLEServerCallbacks {
+  void onConnect(BLEServer* pServer) {
+    deviceConnected = true;
+    sendingFile = true;
+  }
+
+  void onDisconnect(BLEServer* pServer) {
+    deviceConnected = false;
+  }
+};
+
+class MyCharacteristicCallbacks : public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic* pCharacteristic) {
+        std::string value = pCharacteristic_2->getValue();
+        lastAckData = String(value.c_str());
+    }
+};
+
+bool waitForAck() {
+    // Wait for acknowledgment (timeout after 5 seconds)
+    unsigned long startTime = millis();
+    while (millis() - startTime < 5000) {
+        if (lastAckData != NULL) {
+            return true;
+        }
+        delay(10);  // Adjust the delay as needed
+    }
+
+    // Timeout, no acknowledgment received
+    return false;
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  if (!SPIFFS.begin(true)) {
+    Serial.println("Failed to mount SPIFFS");
+    return;
+  }
+
+  File root = SPIFFS.open("/");
+  file = root.openNextFile();
+
+  BLEDevice::init("ESP32");
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  pCharacteristic = pService->createCharacteristic(
+                      CHARACTERISTIC_UUID,
+                      BLECharacteristic::PROPERTY_READ |
+                      BLECharacteristic::PROPERTY_NOTIFY |
+                      BLECharacteristic::PROPERTY_WRITE
+                    );
+
+  pCharacteristic_2 = pService->createCharacteristic(
+                      CHARACTERISTIC_UUID_2,
+                      BLECharacteristic::PROPERTY_READ   |
+                      BLECharacteristic::PROPERTY_WRITE  |                      
+                      BLECharacteristic::PROPERTY_NOTIFY
+                    ); 
+      
+  pBLE2902 = new BLE2902();
+  pBLE2902->setNotifications(true);
+  pCharacteristic->addDescriptor(pBLE2902);
+
+  pBLE2902_2 = new BLE2902();
+  pBLE2902_2->setNotifications(true);
+  pCharacteristic_2->setCallbacks(new MyCharacteristicCallbacks());
+  pCharacteristic_2->addDescriptor(pBLE2902_2);
+
+  // Start the service
+  pService->start();
+
+  // Start advertising
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(false);
+  pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
+  BLEDevice::startAdvertising();
+  Serial.println("Waiting a client connection to notify...");
+}
+
+void loop() {
+  if (deviceConnected) {
+    Serial.println("Client connectted.");
+    if (sendingFile) {
+      // Open the file
+      if (!file) {
+        Serial.println("Failed to open file");
+        return;
+      }
+
+      startMillis_1 = millis();
+      // Read and send the file contents
+      while (file.available()) {
+        uint8_t buffer[251]; // Read in chunks of 251 bytes
+        size_t bytesRead = file.read(buffer, 20);
+
+        // Add error-checking mechanism here (CRC, checksum, etc.)
+
+        pCharacteristic->setValue(buffer, bytesRead);
+        pCharacteristic->notify();
+
+        // Wait for acknowledgment
+        if (!waitForAck()) {
+           // Handle retransmission
+           retransmissionCount++;
+           if (retransmissionCount <= MAX_RETRANSMISSIONS) {
+              // Retransmit the same chunk
+              Serial.println("Retransmitting chunk...");
+              continue;
+           } else {
+              Serial.println("Max retransmissions reached. Aborting.");
+              break;
+           }
+        }
+
+        // Check the received ACK data before continuing
+        if (lastAckData == "1") {
+           // ACK indicates that the client is ready for the next chunk
+           Serial.println("Received ACK. Continuing to the next chunk.");
+        } else {
+           // ACK indicates an error or other condition, handle accordingly
+           Serial.println("Received non-1 ACK. Aborting.");
+           break;
+        }
+        // Reset retransmission count upon successful acknowledgment
+        retransmissionCount = 0;
+      }
+
+      // Close the file
+      file.close();
+      sendingFile = false;
+
+      Serial.print("Total time sent: ");
+      Serial.println(endMillis - startMillis_1);
+    }
+  }
+
+  if (!deviceConnected && oldDeviceConnected) {
+    delay(500); // Give the client time to disconnect
+    pServer->startAdvertising(); // Restart advertising
+    Serial.println("Waiting for a client connection...");
+    oldDeviceConnected = deviceConnected;
+  }
+
+  if (deviceConnected && !oldDeviceConnected) {
+    // do stuff here on connection
+    oldDeviceConnected = deviceConnected;
+  }
+
+  delay(1000);
+}
