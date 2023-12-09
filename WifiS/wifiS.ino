234ab71478f4c670c1b6ed824b1e6f9051fce562
HieuDinh3322 <145694591+HieuDinh3322@users.noreply.github.com>
2023-12-09 14:17:01 +0700

    Update wifiS.ino

--- WifiS/wifiS.ino
+++ WifiS/wifiS.ino
@@ -1,16 +1,34 @@
 #include <WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiServer.h>
 #include <SPIFFS.h>
 
 const char *ssid = "FREE-WIFI";
 const char *password = "20012005";
-const char* serverAddress = "192.168.1.1";
-const int serverPort = 8888;
-WiFiClient client;
+const char* serverAddress = "192.168.1.13"; 
+const int serverPort = 8888; 
+
+WiFiClient ESPclient;
+int retransmissionCount = 0;
+const int MAX_RETRANSMISSIONS = 3;
 
 unsigned long startMillis;
 bool waitingForAck = false;
 size_t lastBytesSent = 0;
 File file;
+
+bool ACK(){
+  if (ESPclient.available()) {
+    char ack = ESPclient.read();
+    if (ack == 'A') {
+      waitingForAck = false;
+      Serial.println("Recieved");
+      // delay(1);
+      return false;
+    }
+  }
+  return true;
+}
 
 void setup() {
   Serial.begin(115200);
@@ -25,60 +43,65 @@ void setup() {
 
   Serial.println("Connecting to WiFi...");
   WiFi.begin(ssid, password);
+  
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting...");
   }
-  Serial.println("Connected to WiFi");
-  Serial.println(WiFi.localIP());
+  
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("Connected to WiFi");
+    Serial.println(WiFi.localIP());
+  }
 
-  // Kết nối đến máy chủ (ESP32 #1)
-  if (client.connect(serverAddress, serverPort)) {
-    Serial.println("Connected to server");
+  if (ESPclient.connect(serverAddress, serverPort)) {
+    Serial.println("Connected to ESP server");
   } else {
-    Serial.println("Connection to server failed");
+    Serial.println("Failed to connect to ESP server");
   }
 }
 
 void loop() {
-  if (client.connected()) {
+  if (ESPclient.connected()) {
     if (!file) {
       Serial.println("Failed to open file");
-      client.stop();
+      ESPclient.stop();
       return;
     }
     
-    // Send a frame only if not waiting for ACK
-    uint8_t buffer[1460];
+  // Read and send data in a loop
+  uint8_t buffer[1460];
+  Serial.println("Start");
+  while (file.available() || waitingForAck) {
+    // Check if there is new data to send
     if (!waitingForAck) {
-        // Adjust the size as needed
-      
       size_t bytesRead = file.read(buffer, sizeof(buffer));
-      client.write(buffer, bytesRead);
       lastBytesSent = bytesRead;
+      // Send data to the client
+      ESPclient.write(buffer, bytesRead);
       waitingForAck = true;
-    }
 
-    // Check for ACK
-    if (client.available()) {
-      char ack = client.read();
-      if (ack == 'A') {
-        waitingForAck = false;
-      }
+      // Record the start time for timeout
+      startMillis = millis();
     }
 
     // Check for timeout (assuming 1000 ms timeout for simplicity)
-    if (waitingForAck && (millis() - startMillis > 1000)) {
+    while (ACK() && (millis() - startMillis > 1000)) {
+      if(retransmissionCount == MAX_RETRANSMISSIONS)
+        break;
       Serial.println("Timeout! Retransmitting...");
-      client.write(buffer, lastBytesSent);
-      waitingForAck = false;
+      ESPclient.write(buffer, lastBytesSent);
+      // Reset the timeout start time
+      startMillis = millis();
+      retransmissionCount++;
     }
-  }
 
-  // Check if the entire file has been sent
-  if (file.available() == 0 && !waitingForAck) {
-    Serial.println("File sent successfully");
-    client.stop();
+    retransmissionCount = 0;
+  }
+ 
+    // Close the file and the connection
     file.close();
+    ESPclient.stop();
+    Serial.println("Client disconnected");
   }
 }
