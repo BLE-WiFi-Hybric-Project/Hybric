0f4ddd43a2c4865613bb3cb5a77acc7e869c7936
HieuDinh3322 <145694591+HieuDinh3322@users.noreply.github.com>
2023-12-12 01:23:27 +0700

    Update wifiS.ino

--- WifiS/wifiS.ino
+++ WifiS/wifiS.ino
@@ -1,107 +1,55 @@
+#include <Arduino.h>
 #include <WiFi.h>
-#include <WiFiClient.h>
-#include <WiFiServer.h>
+#include <ESPAsyncWebServer.h>
 #include <SPIFFS.h>
 
 const char *ssid = "FREE-WIFI";
 const char *password = "20012005";
-const char* serverAddress = "192.168.1.13"; 
-const int serverPort = 8888; 
+String fileName = "/example.txt";
 
-WiFiClient ESPclient;
-int retransmissionCount = 0;
-const int MAX_RETRANSMISSIONS = 3;
+AsyncWebServer server(80);
 
-unsigned long startMillis;
-bool waitingForAck = false;
-size_t lastBytesSent = 0;
-File file;
-
-bool ACK(){
-  if (ESPclient.available()) {
-    char ack = ESPclient.read();
-    if (ack == 'A') {
-      waitingForAck = false;
-      Serial.println("Recieved");
-      // delay(1);
-      return false;
-    }
-  }
-  return true;
-}
-
-void setup() {
+void setup()
+{
   Serial.begin(115200);
 
-  if (!SPIFFS.begin(true)) {
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.print("...");
+  }
+  Serial.println("\nConnected to WiFi");
+  Serial.print(WiFi.localIP());
+
+  // Initialize SPIFFS
+  if (!SPIFFS.begin())
+  {
     Serial.println("Failed to mount SPIFFS");
     return;
   }
 
-  File root = SPIFFS.open("/");
-  file = root.openNextFile();
+  // Route to handle file download
+  server.on("/download", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
+    String fileName = "/example.txt";
+    File file = SPIFFS.open(fileName, "r");
+    if (file) {
+      request->send(SPIFFS, fileName, "text/plain");
+      file.close();
+    } else {
+      request->send(404, "text/plain", "File not found");
+    } });
 
-  Serial.println("Connecting to WiFi...");
-  WiFi.begin(ssid, password);
-  
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting...");
-  }
-  
-  if (WiFi.status() == WL_CONNECTED) {
-    Serial.println("Connected to WiFi");
-    Serial.println(WiFi.localIP());
-  }
+  // Serve other static files (like HTML, CSS, etc.)
+  server.serveStatic("/", SPIFFS, "/").setDefaultFile("index.html");
 
-  if (ESPclient.connect(serverAddress, serverPort)) {
-    Serial.println("Connected to ESP server");
-  } else {
-    Serial.println("Failed to connect to ESP server");
-  }
+  // Start server
+  server.begin();
 }
 
-void loop() {
-  if (ESPclient.connected()) {
-    if (!file) {
-      Serial.println("Failed to open file");
-      ESPclient.stop();
-      return;
-    }
-    
-  // Read and send data in a loop
-  uint8_t buffer[1460];
-  Serial.println("Start");
-  while (file.available() || waitingForAck) {
-    // Check if there is new data to send
-    if (!waitingForAck) {
-      size_t bytesRead = file.read(buffer, sizeof(buffer));
-      lastBytesSent = bytesRead;
-      // Send data to the client
-      ESPclient.write(buffer, bytesRead);
-      waitingForAck = true;
-
-      // Record the start time for timeout
-      startMillis = millis();
-    }
-
-    // Check for timeout (assuming 1000 ms timeout for simplicity)
-    while (ACK() && (millis() - startMillis > 1000)) {
-      if(retransmissionCount == MAX_RETRANSMISSIONS)
-        break;
-      Serial.println("Timeout! Retransmitting...");
-      ESPclient.write(buffer, lastBytesSent);
-      // Reset the timeout start time
-      startMillis = millis();
-      retransmissionCount++;
-    }
-
-    retransmissionCount = 0;
-  }
- 
-    // Close the file and the connection
-    file.close();
-    ESPclient.stop();
-    Serial.println("Client disconnected");
-  }
+void loop()
+{
+  // Nothing to do here
 }
