8a63f76af207bff7d2ad6884855f65e35975e7df
HieuDinh3322 <145694591+HieuDinh3322@users.noreply.github.com>
2023-12-13 01:43:44 +0700

    Update WifiR.ino

--- WifiR/WifiR.ino
+++ WifiR/WifiR.ino
@@ -1,11 +1,11 @@
-#include <Arduino.h>
+// #include <Arduino.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <SPIFFS.h>
 
-const char *ssid = "FREE-WIFI";
-const char *password = "20012005";
-const char *serverIP = "192.168.1.12";
+const char *ssid = "ESP32";
+const char *password = "88888888";
+const char *serverIP = "192.168.4.1";
 
 void setup()
 {
@@ -35,6 +35,7 @@ void loop()
     // Send a GET request to the server to download the file
     HTTPClient http;
     String url = "http://" + String(serverIP) + "/download";
+    String url2 = "http://" + String(serverIP) + "/ack";
 
     Serial.println("Sending GET request to: " + url);
 
@@ -57,16 +58,18 @@ void loop()
             else
                 Serial.println("Error opening file for writing");
             Serial.println("--------------------Finish download--------------------");
+
+            // Send ACK to Server
+            http.begin(url2);
+            int httpCode2 = http.GET();
+            if (httpCode > 0)
+                Serial.println("Send ACK");
         }
         else
-        {
             Serial.printf("Failed to download file, error: %s\n", http.errorToString(httpCode).c_str());
-        }
     }
     else
-    {
         Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-    }
 
     http.end();
 }
