54cf905f9a962f9e6082a9c5ed5a0f72ab2f8bed
HieuDinh3322 <145694591+HieuDinh3322@users.noreply.github.com>
2024-01-16 18:12:55 +0700

    Update main.h

--- hybridClient/include/main.h
+++ hybridClient/include/main.h
@@ -8,6 +8,7 @@ extern File fileSend;
 extern File root;
 extern bool switchToWiFi;
 extern bool fileOpenForUpload;
+extern bool fileBleSend;
 extern int retransmissionCount;
 extern const int MAX_RETRANSMISSIONS;
 
